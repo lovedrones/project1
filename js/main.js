e4e95d0701ac8946ff318aab1be29dae1d85d5ca
lovedrones <JuanTron@Juans-MacBook-Pro.local>
2019-11-04 10:20:15 -0600

    js eventlisteners added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
 /*----- constants -----*/
-/*----- app's state (variables) -----*/
-/*----- cached element references -----*/
-/*----- event listeners -----*/
-/*----- functions -----*/
+const answerBtn = document.getElementById('btn1')
 
+answerBtn.addEventListener('click', answers)
+
+/*----- app's state (variables) -----*/
+
+
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+
+/*----- functions -----*/
+function next() {
+
+};
+
+function dialog() {
+
+};
+
+function question() {
+
+};
+
+function answers() {
+console.log('answered');
+};
+
+function health() {
+    
+}
